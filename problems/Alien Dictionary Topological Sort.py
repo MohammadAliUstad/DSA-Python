4db80f9fe70527de888999a8e0e03a923d35ac55
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2025-11-04 23:34:54 +0530

    Alien Dictionary Topological Sort

--- problems/Alien Dictionary Topological Sort.py
+++ problems/Alien Dictionary Topological Sort.py
@@ -2,7 +2,7 @@ from collections import defaultdict, deque
 
 def alien_order(words):
     graph = defaultdict(set)
-    indeg = {c:0 for w in words for c in w}
+    index = {c:0 for w in words for c in w}
     for i in range(len(words)-1):
         w1,w2 = words[i], words[i+1]
         minlen = min(len(w1), len(w2))
@@ -12,15 +12,15 @@ def alien_order(words):
             if w1[j]!=w2[j]:
                 if w2[j] not in graph[w1[j]]:
                     graph[w1[j]].add(w2[j])
-                    indeg[w2[j]]+=1
+                    index[w2[j]]+=1
                 break
-    q = deque([c for c in indeg if indeg[c]==0])
+    q = deque([c for c in index if index[c]==0])
     res = []
     while q:
         c = q.popleft()
         res.append(c)
         for nei in graph[c]:
-            indeg[nei]-=1
-            if indeg[nei]==0:
+            index[nei]-=1
+            if index[nei]==0:
                 q.append(nei)
-    return "".join(res) if len(res)==len(indeg) else ""
+    return "".join(res) if len(res)==len(index) else ""
